237880d94709fca3cd677151a5b0993311a39e3d
Katchie <41813266+Ka-Da-Sci@users.noreply.github.com>
2025-02-06 16:21:05 +0100

    Implement Email normalizing and ensure that value-error is raised when there is attempt to create a user without email address.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,3 +18,23 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test that the email for a new user is normalized."""
+
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.com', 'test4@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample0987')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email(self):
+        """Test creating user without email raises error."""
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'saample5436')
