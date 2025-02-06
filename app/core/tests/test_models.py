c6c88001b2a58f6abe447e070c8406e3e755991e
Katchie <41813266+Ka-Da-Sci@users.noreply.github.com>
2025-02-06 15:08:05 +0100

    Add unit test for testing successful user creation with email; add feature for the database models User and UserManager for creating and managing users on the system.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,19 @@
+"""Tests for models"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    """Test models"""
+
+    def test_create_user_with_email_success(self):
+        """Test that creating a user with an email is successful."""
+
+        email = "karl@example.com"
+        password = "pass12345"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
